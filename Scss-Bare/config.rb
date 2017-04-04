fb121925b89ef2d1437fa69fb36c7d01818767bc
Birnou <bsebarte@puce-et-media.com>
2017-04-04 13:08:50 +0200

    Add files via upload

---
+++ Scss-Bare/config.rb
@@ -0,0 +1,13 @@
+require 'susy'
+require 'breakpoint'
+
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "imgs"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+relative_assets = true
+line_comments = false
+output_style = :compressed 
